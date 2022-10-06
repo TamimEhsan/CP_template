40a0e07a30f3b2839a1b83fd116830bec5069276
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-06 23:27:56 +0600

    Update KMP.cpp

--- ONSITE_template_final/Onsite templates Shafi/KMP.cpp
+++ ONSITE_template_final/Onsite templates Shafi/KMP.cpp
@@ -8,8 +8,8 @@ void build_failure(string &str){
     for(i=2;i<=str.length();i++){
         cur=i-1;
         while(cur!=0){
-           if(str[failure[cur]]==str[i-1]){
-              failure[i]=failure[cur]+1;
+           if(str[failure[cur]] == str[i-1]){
+              failure[i]= failure[cur]+1;
               break;}
            cur=failure[cur];}}}
 int kmp(string &text,string &pat){
@@ -19,10 +19,24 @@ int kmp(string &text,string &pat){
     for(i=0;i<text.length();i++){
         if(cur==pat.length()){
             ocur++;}
-        while(cur and text[i]!=pat[cur])
-                cur=failure[cur];
+        while(cur and text[i]!= pat[cur])
+                cur= failure[cur];
         if(text[i]==pat[cur])
             cur++;}
     if(cur==pat.length())
         ocur++;
     return ocur;}
+void z_function(const string& str, vector<int>& z){
+    int sz=str.size();
+    z[0]=0;
+    int i,l=0,r=0,j,k;
+    for(i=1;i<sz;i++){
+        z[i]=0;
+        if(i<=r){
+            z[i]=min(r-i+1, z[i-l]);
+        }
+        while(i+z[i]<sz&&str[z[i]] == str[i+z[i]])z[i]++;
+        if(i+z[i]-1>r){
+            l=i;
+            r=i+z[i]-1;}}
+    return;}
