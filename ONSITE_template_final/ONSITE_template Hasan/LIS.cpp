a487995abc427f997a54a006b01df43d24b3ed96
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-06 21:50:36 +0600

    Update LIS.cpp

--- ONSITE_template_final/ONSITE_template Hasan/LIS.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/LIS.cpp
@@ -1,27 +1,3 @@
-int LIS(vector<int> & a)
-{
-    int n=a.size();
-    int inf=1e9+1;
-    vector<int> dp(n+1,inf);
-    dp[0]=-inf;
-    int i;
-    for(i=0;i<n;i++)
-    {
-        int j=upper_bound(dp.begin(),dp.end(),a[i])-dp.begin();
-        if(dp[j-1]<a[i]&&a[i]<dp[j])
-        {
-            dp[j]=a[i];
-        }
-    }
-    int ans=0;
-    for(i=0;i<=n;i++)
-    {
-        if(dp[i]<inf)
-            ans=i;
-    }
-    return ans;
-}
-
 /********************************************/
 ///out[i] contain length of longest increasing sequence
 ///ending at index i(0 based)
