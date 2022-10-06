1487e40115871e868f5b7c1cb494f3e8885bb7d0
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-06 23:11:08 +0600

    Update BIT.cpp

--- ONSITE_template_final/Onsite templates Shafi/BIT.cpp
+++ ONSITE_template_final/Onsite templates Shafi/BIT.cpp
@@ -1,5 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
 const int MaxIdx=1e+5;
 int tree[MaxIdx+1];
 int read(int idx) {int sum = 0;
@@ -18,3 +16,30 @@ int findG(int cumFre) {int idx = 0;int bitMask=(1<<16);
     if (cumFre >= tree[tIdx]) {idx = tIdx;cumFre -= tree[tIdx];}}
     if (cumFre != 0)return -1;else return idx;}
 
+///2d version
+#define MX_SIZE 1000
+ll A[MX_SIZE+1][MX_SIZE+1];
+void add(int i,int j,ll val)
+{
+    int k;
+    for(;i<=MX_SIZE;i+=LSB(i))
+    {
+        for(k=j;k<=MX_SIZE;k+=LSB(k))
+        {
+            A[i][k]+=val;
+        }
+    }
+}
+ll sum(int i,int j)
+{
+    int k;
+    ll s=0;
+    for(;i>0;i-=LSB(i))
+    {
+        for(k=j;k>0;k-=LSB(k))
+        {
+            s+=A[i][k];
+        }
+    }
+    return s;
+}
