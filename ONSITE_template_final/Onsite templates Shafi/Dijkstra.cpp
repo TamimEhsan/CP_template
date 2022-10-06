8622118666f567f8731c56dc8b1fd4c0aa89343e
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-06 23:57:31 +0600

    Update Dijkstra.cpp

--- ONSITE_template_final/Onsite templates Shafi/Dijkstra.cpp
+++ ONSITE_template_final/Onsite templates Shafi/Dijkstra.cpp
@@ -18,3 +18,11 @@ void dijkstra(int src,int n){
                   dist[itr.first]= dist[centre]+ itr.second;
                   cur_dist.push( make_pair(dist[itr.first], itr.first));}}
            vis[centre]=1;}}
+ll g[500][500];
+void floyedWarshal(int n){
+    //g[i][i] should be zero
+    for(int k=0;k<n;k++){
+    for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+       if(g[i][k]<LLONG_MAX&&g[k][j]<LLONG_MAX)
+         g[i][j]=min(g[i][j], g[i][k]+ g[k][j]);}}}}
