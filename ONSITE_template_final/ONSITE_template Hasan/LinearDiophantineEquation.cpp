757129b8702b8fe22d035b841799af4149c1c1da
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-07 13:16:44 +0600

    Update LinearDiophantineEquation.cpp

--- ONSITE_template_final/ONSITE_template Hasan/LinearDiophantineEquation.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/LinearDiophantineEquation.cpp
@@ -16,49 +16,40 @@ template <class T>pair<T,T> extended_euclid(T n,T m){//returns <u,v> such that n
 ///asuming the line equation in form ax+by=c
 ///assuming at least one of a or b is not zero,check it before passing to this function
 ll shiftx(ll x,ll refx,ll q){
-  if(x==refx)
-    return x;
+  if(x==refx) return x;
   if(x<refx){
     ll d=refx-x;
-    if(d%q){
-      d=d-d%q+q;}
+    if(d%q){ d=d-d%q+q;}
     return x+d;}
-  else{
-    ll d=x-refx;
+  else{ll d=x-refx;
     if(d%q)
       d-=d%q;
     return x-d;}}
 ll solveDiophantine(ll a,ll b,ll c,ll x1,ll x2,ll y1,ll y2){
   if(a==0){
-    if(abs(c)%abs(b))
-      return 0;
+    if(abs(c)%abs(b))return 0;
     ll y=c/b;
     if(y>=y1&&y<=y2)
       return (x2-x1+1);
     return 0;}
   if(b==0){
-    if(abs(c)%abs(a))
-      return 0;
+    if(abs(c)%abs(a)) return 0;
     ll x=c/a;
     if(x>=x1&&x<=x2)
       return (y2-y1+1);
     return 0;}
   ll g=__gcd(abs(a),abs(b));
-  if(abs(c)%g){
-    return 0;}
+  if(abs(c)%g){return 0;}
   pair<ll,ll> sol= extended_euclid(abs(a),abs(b));
-  if(a<0){
-    a=-a; b=-b; c=-c;}
-  if(b<0){
-    sol.second=-sol.second;}
+  if(a<0){a=-a; b=-b; c=-c;}
+  if(b<0){sol.second=-sol.second;}
   sol.first*=(c/g);
   sol.second*=(c/g);
   ll x1p,x2p;
   //slope is positive
   if(b<0){
     x1p=floor(1.0*(c-b*y1)/a);
-    if((c-b*y1)%a!=0)
-      x1p++;
+    if((c-b*y1)%a!=0)x1p++;
     x2p=floor(1.0*(c-b*y2)/a);}
   //slope is negative
   else{
@@ -69,13 +60,11 @@ ll solveDiophantine(ll a,ll b,ll c,ll x1,ll x2,ll y1,ll y2){
   ll x11,x22;
   x11=max(x1,x1p);
   x22=min(x2,x2p);
-  if(x22<x11)
-    return 0;
+  if(x22<x11)return 0;
   ll lx,rx;
   lx=shiftx(sol.first,x11,abs(b/g));
   rx=shiftx(sol.first,x22,abs(b/g));
-  if(rx!=x22){
-    rx-=abs(b/g);}
+  if(rx!=x22){rx-=abs(b/g);}
   return max((rx-lx)/(abs(b/g))+ 1, 0ll); }
 void solvecases(int cse){
   ll a,b,c,x1,x2,y1,y2;
