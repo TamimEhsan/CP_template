6f38ded6de456bc61488c36e0e1795c6faefb814
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:28:04 +0600

    Update suffixAutomation.cpp

--- ONSITE_template_final/ONSITE_template Hasan/suffixAutomation.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/suffixAutomation.cpp
@@ -10,34 +10,26 @@ namespace SuffixAut{
     SNode(){
       memset(nxt,-1,sizeof(nxt));
       sufLink=-1;
-      len=0;
-    }
+      len=0;}
     void clear(){
       memset(nxt,-1,sizeof(nxt));
       sufLink=-1;
       len=0;
-      terminal=false;
-    }
-  };
+      terminal=false;}};
   SNode nodes[MaxNode];
   int curNode=1;
   int lastNode=0;
   void init(){
     for(int i=0;i<curNode;++i){
-      nodes[i].clear();
-    }
+      nodes[i].clear();}
     curNode=1;
-    lastNode=0;
-  }
+    lastNode=0;}
   void markTerminal(int val=true){
     int lst=lastNode;
     while(lst!=-1){
       nodes[lst].terminal=val;
-      lst=nodes[lst].sufLink;
-    }
-  }
+      lst=nodes[lst].sufLink;}}
   void extend(char c){
-
     int lst=lastNode;
     int newNode=lastNode=curNode++;
     nodes[newNode].len= nodes[lst].len+1;
@@ -47,17 +39,14 @@ namespace SuffixAut{
         nodes[lst].nxt[c]=newNode;
       else
         break;
-      lst=nodes[lst].sufLink;
-    }
+      lst=nodes[lst].sufLink;}
     if(lst==-1){
       nodes[newNode].sufLink=0;
-      return;
-    }
+      return;}
     int p=lst,q=nodes[lst].nxt[c];
     if(nodes[p].len+1== nodes[q].len){
       nodes[newNode].sufLink=q;
-      return;
-    }
+      return;}
     int clone=curNode++;
     nodes[clone]=nodes[q];
     nodes[clone].len=nodes[p].len+1;
@@ -68,27 +57,19 @@ namespace SuffixAut{
         nodes[p].nxt[c]=clone;
       else
         break;
-      p=nodes[p].sufLink;
-    }
-  }
+      p=nodes[p].sufLink;}}
   void build(string s){
     init();
     for(auto e:s)
       extend(e);
-    markTerminal();
-  }
+    markTerminal();}
   void print(){
     for(int i=0;i<curNode;++i){
       cout<<"Node "<<i<<":\n";
       for(int j=0;j<AS;++j){
         if(nodes[i].nxt[j]!=-1)
         {
-          cout<<(char)(j+offset)<<':'<<nodes[i].nxt[j]<<'\n';
-        }
-      }
+          cout<<(char)(j+offset)<<':'<<nodes[i].nxt[j]<<'\n';}}
       cout<<"len "<<nodes[i].len<<'\n';
       cout<<"sufLink "<<nodes[i].sufLink<<'\n';
-      cout<<"terminal "<<nodes[i].terminal<<'\n';
-    }
-  }
-}
+      cout<<"terminal "<<nodes[i].terminal<<'\n';}}}
