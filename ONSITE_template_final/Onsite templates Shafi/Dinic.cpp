aebb626f34e974ca5f952c64cd9157a97e7db4c2
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:17:43 +0600

    bracket optimization

--- ONSITE_template_final/Onsite templates Shafi/Dinic.cpp
+++ ONSITE_template_final/Onsite templates Shafi/Dinic.cpp
@@ -1,9 +1,7 @@
 struct FlowEdge {
   int v, u;
   long long cap, flow = 0;
-  FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
-};
-
+  FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}};
 class Dinic {
   const long long flow_inf = 1e18;
   vector<FlowEdge> edges;
@@ -16,18 +14,14 @@ class Dinic {
   Dinic(int n, int s, int t) : n(n), s(s), t(t) {
     adj.resize(n);
     level=new int[n];
-    ptr=new int[n];
-  }
-
-  void add_edge(int v, int u, long long cap) {
+    ptr=new int[n];}
+void add_edge(int v, int u, long long cap) {
     edges.emplace_back(v, u, cap);
     edges.emplace_back(u, v, 0);
     adj[v].push_back(m);
     adj[u].push_back(m + 1);
-    m += 2;
-  }
-
-  bool bfs() { //builds level graph
+    m += 2;}
+bool bfs() { //builds level graph
     while (!q.empty()) {
       int v = q.front();
       q.pop();
@@ -37,13 +31,9 @@ class Dinic {
         if (level[edges[id].u] != -1)
           continue;
         level[edges[id].u] = level[v] + 1;
-        q.push(edges[id].u);
-      }
-    }
-    return level[t] != -1;
-  }
-
-  long long dfs(int v, long long pushed) {  //pushes flow through level graph
+        q.push(edges[id].u);}}
+    return level[t] != -1;}
+long long dfs(int v, long long pushed) {  //pushes flow through level graph
     if (pushed == 0)
       return 0;
     if (v == t)
@@ -58,12 +48,9 @@ class Dinic {
         continue;
       edges[id].flow += tr;
       edges[id ^ 1].flow -= tr;
-      return tr;
-    }
-    return 0;
-  }
-
-  long long flow() {
+      return tr;}
+    return 0;}
+long long flow() {
     long long f = 0;
     while (true) {
       memset(level, -1, sizeof level);
@@ -73,9 +60,5 @@ class Dinic {
         break;
       memset(ptr, 0, sizeof ptr);
       while (long long pushed = dfs(s, flow_inf)) {
-        f += pushed;
-      }
-    }
-    return f;
-  }
-};
+        f += pushed;}}
+    return f;}};
