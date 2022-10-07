8ccfb35f605c34af82f5f2d7f8442287333da950
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:20:45 +0600

    bracket optimization

--- ONSITE_template_final/Onsite templates Shafi/LCA.cpp
+++ ONSITE_template_final/Onsite templates Shafi/LCA.cpp
@@ -4,25 +4,18 @@ const int MAXN=100001, LOG=20;
 vector<vector<int>> g;
 int n;
 int bparent[MAXN][LOG], depth[MAXN],vis[MAXN];
-
-//build depth[] and immediate ancestor
 void dfs(int a){
   vis[a]=1;
   for(auto v: g[a]){
     if(!vis[v]){
       bparent[v][0]=a;
       depth[v]=1+depth[a];
-      dfs(v);
-    }
-  }
-}
-//build bparent[]
+      dfs(v);}}}
 void build_ancestor(){
   dfs(1);
   for(int i=1;(1<<i)<n;i++)
     for(int j=1;j<=n;j++)
-      bparent[j][i]=bparent[bparent[j][i-1]][i-1];
-}
+      bparent[j][i]=bparent[bparent[j][i-1]][i-1];}
 int pth_ancestor(int a,int p){
   for(int i=0;(1<<i)<=p;i++)
     if((1<<i)&p)
@@ -40,8 +33,5 @@ int lca(int u,int v){
   for(int i=log2(n-1);i>=0;i--){
     if(bparent[u][i]!=bparent[v][i]){
       u=bparent[u][i];
-      v=bparent[v][i];
-    }
-  }
-  return bparent[u][0];
-}
+      v=bparent[v][i];}}
+  return bparent[u][0];}
