68790f44d5f6ee245abc0810ed24204645a4ca81
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:32:43 +0600

    bracket optimization

--- ONSITE_template_final/ONSITE_template Hasan/onlineBridge.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/onlineBridge.cpp
@@ -2,7 +2,6 @@ vector<int> par, dsu_2ecc, dsu_cc, dsu_cc_size;
 int bridges;
 int lca_iteration;
 vector<int> last_visit;
-
 void init(int n) {
   par.resize(n);
   dsu_2ecc.resize(n);
@@ -14,22 +13,16 @@ void init(int n) {
     dsu_2ecc[i] = i;
     dsu_cc[i] = i;
     dsu_cc_size[i] = 1;
-    par[i] = -1;
-  }
-  bridges = 0;
-}
+    par[i] = -1;}
+  bridges = 0;}
 
 int find_2ecc(int v) {
   if (v == -1)
     return -1;
-  return dsu_2ecc[v] == v ? v : dsu_2ecc[v] = find_2ecc(dsu_2ecc[v]);
-}
-
+  return dsu_2ecc[v] == v ? v : dsu_2ecc[v] = find_2ecc(dsu_2ecc[v]);}
 int find_cc(int v) {
   v = find_2ecc(v);
-  return dsu_cc[v] == v ? v : dsu_cc[v] = find_cc(dsu_cc[v]);
-}
-
+  return dsu_cc[v] == v ? v : dsu_cc[v] = find_cc(dsu_cc[v]);}
 void make_root(int v) {
   v = find_2ecc(v);
   int root = v;
@@ -39,11 +32,8 @@ void make_root(int v) {
     par[v] = child;
     dsu_cc[v] = root;
     child = v;
-    v = p;
-  }
-  dsu_cc_size[root] = dsu_cc_size[child];
-}
-
+    v = p;}
+  dsu_cc_size[root] = dsu_cc_size[child];}
 void merge_path (int a, int b) {
   ++lca_iteration;
   vector<int> path_a, path_b;
@@ -57,44 +47,32 @@ void merge_path (int a, int b) {
         break;
         }
       last_visit[a] = lca_iteration;
-      a = par[a];
-    }
+      a = par[a];}
     if (b != -1) {
       b = find_2ecc(b);
       path_b.push_back(b);
       if (last_visit[b] == lca_iteration){
         lca = b;
-        break;
-        }
+        break;}
       last_visit[b] = lca_iteration;
-      b = par[b];
-    }
-
-  }
-
-  for (int v : path_a) {
+      b = par[b];}}
+for (int v : path_a) {
     dsu_2ecc[v] = lca;
     if (v == lca)
       break;
-    --bridges;
-  }
+    --bridges;}
   for (int v : path_b) {
     dsu_2ecc[v] = lca;
     if (v == lca)
       break;
-    --bridges;
-  }
-}
-
+    --bridges;}}
 void add_edge(int a, int b) {
   a = find_2ecc(a);
   b = find_2ecc(b);
   if (a == b)
     return;
-
   int ca = find_cc(a);
   int cb = find_cc(b);
-
   if (ca != cb) {
     ++bridges;
     if (dsu_cc_size[ca] > dsu_cc_size[cb]) {
@@ -105,6 +83,4 @@ void add_edge(int a, int b) {
     par[a] = dsu_cc[a] = b;
     dsu_cc_size[cb] += dsu_cc_size[a];
   } else {
-    merge_path(a, b);
-  }
-}
+    merge_path(a, b);}}
