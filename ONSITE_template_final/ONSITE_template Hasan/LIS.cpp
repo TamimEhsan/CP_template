22c84d37cdf815285d1572c501ab6e6656a2449d
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-07 12:18:50 +0600

    Update LIS.cpp

--- ONSITE_template_final/ONSITE_template Hasan/LIS.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/LIS.cpp
@@ -9,11 +9,8 @@ void LIS(vector<int> & a,vector<int>& out){
   dp[0]=-INT_MAX;
   int i;
   for(i=0;i<n;i++){
-    int j=upper_bound(dp.begin(),dp.end(),a[i])-dp.begin();
-    int k=lower_bound(dp.begin(),dp.end(),a[i])-dp.begin();
+    int j=upper_bound(dp.begin(), dp.end(), a[i])-dp.begin();
+    int k=lower_bound(dp.begin(), dp.end(), a[i])-dp.begin();
     out[i]=k;
     if(dp[j-1]<a[i]&&a[i]<dp[j]){
-      dp[j]=a[i];
-    }
-  }
-}
+      dp[j]=a[i];}}}
