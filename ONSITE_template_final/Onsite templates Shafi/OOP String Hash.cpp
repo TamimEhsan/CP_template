39b2ececf64c95c785ae5773bb98125dd1559719
neo11235 <63189687+neo11235@users.noreply.github.com>
2022-10-07 13:10:19 +0600

    Update OOP String Hash.cpp

--- ONSITE_template_final/Onsite templates Shafi/OOP String Hash.cpp
+++ ONSITE_template_final/Onsite templates Shafi/OOP String Hash.cpp
@@ -14,7 +14,7 @@ public:
     if(i1==0)return hf[i2];
     return ((hf[i2]-hf[i1-1]+mod)* ipower[i1])%mod;
   }};
-unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+unsigned seed = chrono ::system_clock ::now(). time_since_epoch(). count();
 linear_congruential_engine<uint_fast32_t, 1002517UL, 1001593UL, 2147483647UL> lcg(seed);
 int primes_for_mod[]={1000019353, 1000001087, 1000020353, 1000003267, 1000000439, 1000018001, 1000019569, 1000020701, 1000016929, 1000007521,
 1000007773, 1000013323, 1000018379, 1000017203, 1000006211, 1000004693, 1000013011, 1000020829, 1000011277, 1000007147};
