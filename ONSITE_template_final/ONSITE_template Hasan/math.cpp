aa46a295590d0ae7c387fccadcea92bef9cc4140
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 14:48:08 +0600

    Update math.cpp

--- ONSITE_template_final/ONSITE_template Hasan/math.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/math.cpp
@@ -132,3 +132,25 @@ for(i=0;prime[i]<=lim;++i){
 inv[1] = 1;
 for(int i = 2; i < m; ++i)
  inv[i] = m - (m/i) * inv[m%i] % m;
+//floor ceil change
+vector<int> where_floor_changes(int n){
+ int now=1;
+ vector<int>v;
+ while(now<=n) {
+  v.push_back(now);
+  now=n/(n/now)+1;}
+ return v;}
+vector<pair<int,int>> where_ceil_changes(int m){
+ vector<pair<int,int>>v;
+ int l=1;
+ while(l<=m) {if(l==m) {
+   v.push_back({m,m});
+   break;}
+  int cl=(m+l-1)/l;
+  int r=(m+cl-2)/(cl-1)-1;
+  r=min(r,m);
+  r=max(r,l);
+  v.push_back({l,r});
+  if(r==m)break;
+        l=r+1;}
+ return v;}
