bf42cc14fe68d32e26959a9e1824cb3dec399eda
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:36:01 +0600

    bracket optimization

--- ONSITE_template_final/ONSITE_template Hasan/miscGeo.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/miscGeo.cpp
@@ -7,36 +7,28 @@ int quarter(pll a){
     if(a.second>=0)
       return 0;
     else
-      return 3;
-  }
+      return 3;}
   else{
     if(a.second>=0)
       return 1;
-    return 2;
-  }
-}
+    return 2;}}
 bool polar_order(pair<pll,int> & a,pair<pll,int>& b){
   if(quarter(a.first)==quarter(b.first)){
     return cross(a.first,b.first)>0;
   }
   else
-    return quarter(a.first)<quarter(b.first);
-}
+    return quarter(a.first)<quarter(b.first);}
 //number of point on a line segment
 template <class T>
 T npoint(pair<T,T> l,pair<T,T> r){
   T x=abs(l.first-r.first);
   T y=abs(l.second-r.second);
   if(x==0){
-    return y+1;
-  }
+    return y+1;}
   else if(y==0){
-    return x+1;
-  }
+    return x+1;}
   T g=__gcd(x,y);
-  return g+1;
-}
-
+  return g+1;}
 //moderately tested on loj and cses
 //crossing number from a point
 //polygon[n]=polygon[0] where n= number of vertex in the original polygon
@@ -53,11 +45,8 @@ int cnPnPolygon(vector<pair<T,T> >& polygon,pair<T,T> pn){
       if(polygon[i+1].second-polygon[i].second<0)
         tmp=-tmp;
       if(tmp>0)
-        cnt++;
-    }
-  }
-  return cnt;
-}
+        cnt++;}}
+  return cnt;}
 template<class T>
 bool onBoundary(vector<pair<T,T> >&polygon,pair<T,T> pn){
   int n=(int)polygon.size();
@@ -69,16 +58,12 @@ bool onBoundary(vector<pair<T,T> >&polygon,pair<T,T> pn){
        &&((pn.second>=polygon[i].second&& pn.second<=polygon[i+1].second)|| (pn.second<=polygon[i].second&& pn.second>=polygon[i+1].second)))
     {
       return true;
-    }
-  }
-  return false;
-}
-
+    }}
+  return false;}
 template <class T>
 inline
 T cross(pair<T,T> p0,pair<T,T> p1,pair<T,T> p2){
-  return (p1.first-p0.first)* (p2.second-p0.second)- (p1.second-p0.second)* (p2.first-p0.first);
-}
+  return (p1.first-p0.first)* (p2.second-p0.second)- (p1.second-p0.second)* (p2.first-p0.first);}
 //winding number from a point,tested on loj timus and cses
 template<class T>
 int wnPnPolygon(vector<pair<T,T> >& polygon,pair<T,T> pn){
@@ -88,14 +73,7 @@ int wnPnPolygon(vector<pair<T,T> >& polygon,pair<T,T> pn){
   for(int i=0;i<n-1;i++){
     if(polygon[i].second<=pn.second){
       if(polygon[i+1].second>pn.second&& cross(pn,polygon[i],polygon[i+1])>0)
-      {
-        ++wn;
-      }
-    }
+      {++wn;}}
     else if(polygon[i+1].second<=pn.second&& cross(pn,polygon[i],polygon[i+1])<0)
-    {
-      --wn;
-    }
-  }
-  return wn;
-}
+    {--wn;}}
+  return wn;}
