ca6a9639bab2c0781a74b5a62039bee5e54b05d5
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:19:13 +0600

    bracket optimization

--- ONSITE_template_final/Onsite templates Shafi/Hungarian.cpp
+++ ONSITE_template_final/Onsite templates Shafi/Hungarian.cpp
@@ -9,56 +9,35 @@
 namespace wm{ /// hash = 581023
   bool visited[MAX];
   int U[MAX], V[MAX], P[MAX], way[MAX], minv[MAX], match[MAX], ar[MAX][MAX];
-
   /// n = number of row and m = number of columns in 1 based, flag = MAXIMIZE or MINIMIZE
   /// match[i] contains the column to which row i is matched
   int hungarian(int n, int m, int mat[3][3], int flag){
     clr(U), clr(V), clr(P), clr(ar), clr(way);
-
     for (int i = 1; i <= n; i++){
       for (int j = 1; j <= m; j++){
         ar[i][j] = mat[i][j];
-        if (flag == MAXIMIZE) ar[i][j] = -ar[i][j];
-      }
-    }
+        if (flag == MAXIMIZE) ar[i][j] = -ar[i][j];}}
     if (n > m) m = n;
-
     int i, j, a, b, c, d, r, w;
     for (i = 1; i <= n; i++){
       P[0] = i, b = 0;
       for (j = 0; j <= m; j++) minv[j] = inf, visited[j] = false;
-
       do{
         visited[b] = true;
         a = P[b], d = 0, w = inf;
-
         for (j = 1; j <= m; j++){
           if (!visited[j]){
             r = ar[a][j] - U[a] - V[j];
             if (r < minv[j]) minv[j] = r, way[j] = b;
-            if (minv[j] < w) w = minv[j], d = j;
-          }
-        }
-
+            if (minv[j] < w) w = minv[j], d = j;}}
         for (j = 0; j <= m; j++){
           if (visited[j]) U[P[j]] += w, V[j] -= w;
-          else minv[j] -= w;
-        }
+          else minv[j] -= w;}
         b = d;
       } while (P[b] != 0);
-
       do{
         d = way[b];
         P[b] = P[d], b = d;
-      } while (b != 0);
-    }
+      } while (b != 0);}
     for (j = 1; j <= m; j++) match[P[j]] = j;
-    return (flag == MINIMIZE) ? -V[0] : V[0];
-  }
-}
-
-int main(){
-  int mat[3][3]={{0,0,0},{0,10,20},{0,30,20}};
-  cout<<wm::hungarian(2,2,mat,1)<<endl;
-  cout<<wm::match[1]<<" "<<wm::match[2]<<endl;
-}
+    return (flag == MINIMIZE) ? -V[0] : V[0];}}
