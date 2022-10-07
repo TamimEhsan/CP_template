cd1475caf00b5d24fad2cbc6b961fa4ab5851455
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:16:05 +0600

    bracket optimization

--- ONSITE_template_final/Onsite templates Shafi/Dijkstra.cpp
+++ ONSITE_template_final/Onsite templates Shafi/Dijkstra.cpp
@@ -16,25 +16,17 @@ void dijkstra(int src,int n){
     for(auto itr: graph[centre]){
       if(dist[centre]+ itr.second< dist[itr.first]){
         dist[itr.first]= dist[centre]+ itr.second;
-        cur_dist.push( make_pair(dist[itr.first], itr.first));
-      }
-    }
-    vis[centre]=1;
-  }
-}
-
+        cur_dist.push( make_pair(dist[itr.first], itr.first));}}
+    vis[centre]=1;}}
 //floyd warshall
 ll g[500][500];
-
 void floyedWarshal(int n){
   //g[i][i] should be zero
   for(int k=0;k<n;k++){
   for(int i=0;i<n;i++){
   for(int j=0;j<n;j++){
     if(g[i][k]<LLONG_MAX&&g[k][j]<LLONG_MAX)
-      g[i][j]=min(g[i][j], g[i][k]+ g[k][j]);}}}
-}
-
+      g[i][j]=min(g[i][j], g[i][k]+ g[k][j]);}}}}
 //bellmen-ford
 int dist[10001];
 struct edge{
@@ -47,6 +39,4 @@ void bell_ford(int src,int n){
    dist[src]=0;
    for(int i=1;i<n;i++){
      for(int j=0;j<graph.size();j++)
-       dist[graph[j].b]=min(dist[graph[j].b],dist[graph[j].a]+graph[j].cost);
-   }
-}
+       dist[graph[j].b]=min(dist[graph[j].b],dist[graph[j].a]+graph[j].cost);}}
