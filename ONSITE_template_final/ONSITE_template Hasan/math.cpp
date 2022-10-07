02587d3e196b4acd21c2633b8a6a105a0b478616
ahshafi <76455395+ahshafi@users.noreply.github.com>
2022-10-07 12:37:14 +0600

    bracket optimization

--- ONSITE_template_final/ONSITE_template Hasan/math.cpp
+++ ONSITE_template_final/ONSITE_template Hasan/math.cpp
@@ -8,24 +8,17 @@ void LinearSeive(int n){
   for(int  i=2;i<n;++i){
     if(lp[i]==0){
       lp[i]=i;
-      prime.push_back(i);
-    }
+      prime.push_back(i);}
     for(int j=0;j<(int)prime.size()&& prime[j]<=lp[i]&&i*prime[j]<n;++j)
-    {
-      lp[i*prime[j]]=prime[j];
-    }
-  }
-}
+    {lp[i*prime[j]]=prime[j];}}}
 ///*******************************************///
 ll modular_exp(ll a,ll b,ll mod){//mod should be <32 bit
   ll ans=1;
   while(b>0){
     if(b&1)ans=(ans*a)%mod;
     b=b>>1;
-    a=(a*a)%mod;
-  }
-  return ans;
-}
+    a=(a*a)%mod;}
+  return ans;}
 /*********finding all factor below n in O(nlogn)*********/
 int *pfactor;
 void build(int n){//prime factor of every number below n
@@ -35,12 +28,8 @@ void build(int n){//prime factor of every number below n
   for(i=2;i<=n;i++){
     if(pfactor[i]==0){
       for(j=i;j<=n;j+=i){
-        pfactor[j]=i;
-      }
-    }
-  }
-  return;
-}
+        pfactor[j]=i;}}}
+  return;}
 int get_p_factor(vector<int>& pf,vector<int>& pfp,int n){//pf and pfp must have size>log(n) returns number of prime factor
   int i=0;
   int j,pw;
@@ -49,14 +38,11 @@ int get_p_factor(vector<int>& pf,vector<int>& pfp,int n){//pf and pfp must have 
     pw=0;
     while(!(n%j)){
       n/=j;
-      pw++;
-    }
+      pw++;}
     pf[i]=j;
     pfp[i]=pw;
-    i++;
-  }
-  return i;
-}
+    i++;}
+  return i;}
 int get_all_factor(vector<int>& pf,vector<int>& pfp,int sz,vector<int> &vct){
   vct[0]=1;
   int i,j,k,l,r,s=1;
@@ -65,18 +51,13 @@ int get_all_factor(vector<int>& pf,vector<int>& pfp,int sz,vector<int> &vct){
     for(j=0;j<pfp[i];j++){
       r=s;
       for(k=l;k<r;k++,s++){
-        vct[s]=(vct[k]*pf[i]);
-      }
-      l=r;
-    }
-  }
-  return s;
-}
+        vct[s]=(vct[k]*pf[i]);}
+      l=r;}}
+  return s;}
 /***********General multiplicative function****************/
 int *mf;
 int base_case(int p,int k){//base case for p^k
-  return k+1;
-}
+  return k+1;}
 void comp_mult_func(int n){
   mf=new int[n+1];
   memset(mf,-1,sizeof(int)*(n+1));
@@ -92,10 +73,7 @@ void comp_mult_func(int n){
       while(l<=(ll)n){
         mf[l]=base_case(i,k);
         k++;
-        l*=i;
-      }
-    }
-  }
+        l*=i;}}}
   for(i=2;i<=n;i++){
     if(mf[i]<0){
       mf[i]=-mf[i];
@@ -103,11 +81,8 @@ void comp_mult_func(int n){
       while(!(k%mf[i])){
         k/=mf[i];
       }
-      mf[i]=mf[k]*mf[i/k];
-    }
-  }
-  return;
-}
+      mf[i]=mf[k]*mf[i/k];}}
+  return;}
 /*********mobius function************/
 int mob[1000001];
 void mobius(){
@@ -115,5 +90,4 @@ void mobius(){
   mob[1]=1;
   for(int i=2;i<1000001;++i)
     for(int j=i*2;j<1000001;j+=i)
-      mob[j]-=mob[i];
-}
+      mob[j]-=mob[i];}
