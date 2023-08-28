946c61a34e710f166867fb97af759b34394cfa74
Tamim Ehsan <tamimehsan99@gmail.com>
2023-08-28 19:44:26 +0600

    28/8/23

--- Graph Theory/Tree/LCA (Binary Lifting).cpp
+++ Graph Theory/Tree/LCA (Binary Lifting).cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+/**
+1 indexed
+*/
 const int MAXN=200005, LOG=20;
 vector<int> adj[MAXN];
-int n;
+
 int bparent[MAXN][LOG], depth[MAXN],vis[MAXN];
 
 void dfs(int a){
@@ -15,9 +19,9 @@ void dfs(int a){
         }
     }
 }
-void build_ancestor(){
+void build_ancestor(int n){
     dfs(1);
-    for(int i=1; (1<<i)<n; i++)
+    for(int i=1; i < LOG; i++)
         for(int j=1; j<=n; j++)
             bparent[j][i]=bparent[bparent[j][i-1]][i-1];
 }
@@ -37,7 +41,7 @@ int lca(int u,int v){
     if(u==v)
         return u;
     //printf("%d %d\n",u,v);
-    for(int i=log2(n-1); i>=0; i--){
+    for(int i=LOG-1; i>=0; i--){
         if(bparent[u][i]!=bparent[v][i]){
             u=bparent[u][i];
             v=bparent[v][i];
@@ -46,15 +50,28 @@ int lca(int u,int v){
     return bparent[u][0];
 }
 
+int dist(int u, int v) {
+    int l = lca(u, v);
+    return depth[u] + depth[v] - 2*depth[l];
+}
+
+// finds if path (s,t) and (u,v) has at least 1 common vertex
+bool check_overlap(int s, int t, int u, int v) {
+    int min_dist = dist(s, t) + dist(u, v);
+    int o1 = dist(s, u) + dist(v, t);
+    int o2 = dist(s, v) + dist(u, t);
+    return !( (min_dist < o1) and (min_dist < o2) );
+}
+
 int main(){
-    int q;
+    int n,q;
     cin>>n>>q;
     for(int i=2;i<=n;i++){
         int p;
         cin>>p;
         adj[p].push_back(i);
     }
-    build_ancestor();
+    build_ancestor(n);
     while(q--){
         int a,b;
         cin>>a>>b;
